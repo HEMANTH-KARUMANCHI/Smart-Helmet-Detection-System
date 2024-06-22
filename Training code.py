911219acd180a84341a2ac9a039f7d6d35a8d976
HEMANTH-KARUMANCHI <84486462+HEMANTH-KARUMANCHI@users.noreply.github.com>
2024-06-23 00:10:00 +0530

    Create Training code.py

---
+++ Training code.py
@@ -0,0 +1,19 @@
+#install pre-installments
+pip install Ultralytics
+
+from ultralytics import YOLO
+model = "yolov8l.pt"
+data = "dataset yaml file location"
+epochs = 100
+imgsz = 640
+
+yolo = YOLO()
+
+yolo.train(
+ task="detect",
+ mode="train",
+ model=model,
+ data=data,
+ epochs=epochs,
+ imgsz=imgsz,
+)
